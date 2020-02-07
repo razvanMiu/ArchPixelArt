9e8c2d202064b2d0e94f6d50507e9e45a8a67caa
Miu Razvan <miu.razvan28@gmail.com>
2020-02-07 09:32:57 -0800

    Update 0-setupmirrors.sh

--- 0-setupmirrors.sh
+++ 0-setupmirrors.sh
@@ -1,3 +1,3 @@
 pacman -S --noconfirm pacman-contrib
 mv /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup
-curl -s "https://www.archlinux.org/mirrorlist/?country=US&protocol=https&use_mirror_status=on" | sed -e 's/^#Server/Server/' -e '/^#/d' | rankmirrors -n 5 - > /etc/pacman.d/mirrorlist
+curl -s "https://www.archlinux.org/mirrorlist/?country=RO&protocol=http&protocol=https&use_mirror_status=on" | sed -e 's/^#Server/Server/' -e '/^#/d' | rankmirrors -n 5 - > /etc/pacman.d/mirrorlist
